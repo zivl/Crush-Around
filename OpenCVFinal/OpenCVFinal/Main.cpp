86798358cc17e3234bea52301c4f82c813f29001
zivl <levi.ziv@gmail.com>
2014-04-04 10:19:38 +0300

    add opencv settings and open an image

--- OpenCVFinal/OpenCVFinal/Main.cpp
+++ OpenCVFinal/OpenCVFinal/Main.cpp
@@ -1,7 +1,19 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
+#include <fstream>
 
+using namespace cv;
 using namespace std;
 
-void main(){
-	cout << "Hello World";
+int main()
+{
+    Mat im = imread("c:\\times.jpg");
+    if (im.empty()) 
+    {
+        cout << "Cannot load image!" << endl;
+        return -1;
+    }
+    imshow("Image", im);
+    waitKey(0);
 }
