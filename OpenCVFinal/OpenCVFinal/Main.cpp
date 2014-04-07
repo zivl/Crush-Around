1ad24ba7c3b0e21c2a94f776865ccccc330abee0
Tomer Cagan <tomer@tomercagan.com>
2014-04-07 10:25:12 +0300

    Modified to use relative path Cosmetic changes

--- OpenCVFinal/OpenCVFinal/Main.cpp
+++ OpenCVFinal/OpenCVFinal/Main.cpp
@@ -14,7 +14,7 @@ using namespace cv;
 using namespace std;
 
 #define MAX_FOLDER_PATH_CHARS 200
-#define INPUT_FOLDER "C:\\vs2012_projects\\Inputs\\"
+#define INPUT_FOLDER "..\\..\\Inputs\\"
 #define IMG_ON_WALL_SPEAKERS "on-wall-speakers.jpg"
 #define IMG_PLANE "plane.jpg"
 #define IMG_CARD "card.jpg"
@@ -24,48 +24,46 @@ using namespace std;
 #define BLOB_MAX_AREA 500.0f
 
 void showBlobs(Mat image){
-	const char *wndNameOut = "Out";
+    const char *wndNameOut = "Out";
 
-        Mat out;
-        vector<KeyPoint> keyPoints;
-        SimpleBlobDetector::Params params;
-		params.filterByColor = false;
-        params.filterByCircularity = false;
+    Mat out;
+    vector<KeyPoint> keyPoints;
+    SimpleBlobDetector::Params params;
+    params.filterByColor = false;
+    params.filterByCircularity = false;
 
-		params.minDistBetweenBlobs = BLOB_DISTANCE_BETWEEEN_BLOBS;
-        params.minThreshold = 40;
-        params.maxThreshold = 100;
-        params.thresholdStep = 10;
+    params.minDistBetweenBlobs = BLOB_DISTANCE_BETWEEEN_BLOBS;
+    params.minThreshold = 40;
+    params.maxThreshold = 100;
+    params.thresholdStep = 10;
 
-        params.minArea = 300; 
-        params.maxArea = 8000;
-                
-		params.minConvexity = 0.3;
-        params.maxConvexity = 10;
+    params.minArea = 300; 
+    params.maxArea = 8000;
 
-		params.minInertiaRatio = 0.01;
+    params.minConvexity = 0.3;
+    params.maxConvexity = 10;
 
+    params.minInertiaRatio = 0.01;
 
-        SimpleBlobDetector blobDetector( params );
-        blobDetector.create("SimpleBlob");
+    SimpleBlobDetector blobDetector( params );
+    blobDetector.create("SimpleBlob");
 
-        blobDetector.detect(image, keyPoints);
-        drawKeypoints(image, keyPoints, out, CV_RGB(255, 0, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    blobDetector.detect(image, keyPoints);
+    drawKeypoints(image, keyPoints, out, CV_RGB(255, 0, 0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
-		for(int i = 0; i < keyPoints.size(); i++) {
-			if(i + 1 < keyPoints.size()){
-				line(out, keyPoints[i].pt, keyPoints[i + 1].pt, CV_RGB(0, 255, 0), 2);
-			}
-			else {
-				line(out, keyPoints[i].pt, keyPoints[0].pt, CV_RGB(0, 255, 0), 2);
-			}
-		}
+    for(int i = 0; i < keyPoints.size(); i++) {
+        if(i + 1 < keyPoints.size()){
+            line(out, keyPoints[i].pt, keyPoints[i + 1].pt, CV_RGB(0, 255, 0), 2);
+        }
+        else {
+            line(out, keyPoints[i].pt, keyPoints[0].pt, CV_RGB(0, 255, 0), 2);
+        }
+    }
 
+    cout << "Keypoints " << keyPoints.size() << endl;
 
-        cout << "Keypoints " << keyPoints.size() << endl;
-        
-        imshow("Blobs", out);
-        waitKey(0);
+    imshow("Blobs", out);
+    waitKey(0);
 }
 
 int main()
@@ -73,7 +71,7 @@ int main()
 	char filename[MAX_FOLDER_PATH_CHARS] = "";
 	
 	strcat_s(filename, MAX_FOLDER_PATH_CHARS, INPUT_FOLDER);
-	strcat_s(filename, MAX_FOLDER_PATH_CHARS, IMG_CARD);
+    strcat_s(filename, MAX_FOLDER_PATH_CHARS, IMG_ON_WALL_SPEAKERS);
 	Mat im = imread(filename);
     if (im.empty()) 
     {
