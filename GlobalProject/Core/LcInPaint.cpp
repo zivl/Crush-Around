988ccd71472b39f06ea4f717602f810034f3e542
zivl <levi.ziv@gmail.com>
2014-05-17 18:56:02 +0300

    remove dead code

--- GlobalProject/Core/LcInPaint.cpp
+++ GlobalProject/Core/LcInPaint.cpp
@@ -2,30 +2,6 @@
 #include "LcInPaint.h"
 
 using namespace cv;
-/*
-void LcInPaint::prepareInpaint(const Mat input){
-    input.copyTo(frame2);
-    input.copyTo(frame3);
-
-    contours = objDetector.getObjectContours(frame2);
-
-    std::vector<Point> approx;
-    // test each contour
-    for( size_t i = 0; i < contours.size(); i++ )
-    {
-        Point* pnts = new Point[contours[i].size()];
-        for (int j = 0; j < contours[i].size(); j++)
-        {
-            pnts[j] = contours[i][j];
-        }
-
-        const Point* ppt[1] = { pnts };
-        int npt[] = { static_cast<int>(contours[i].size()) };
-        fillPoly(frame3, ppt, npt, 1, Scalar(120, 250, 50));
-
-        delete[] pnts;
-    }
-}*/
 
 void LcInPaint::inpaint(const Mat input, std::vector<std::vector<cv::Point>> contours, Mat &output){
     
